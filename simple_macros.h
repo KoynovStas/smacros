5ee712fbeb243b711eda6e5ebab94d2d6f930582
KoynovStas <skojnov@yandex.ru>
2015-05-19 10:45:51 +0300

    Add EXTERN_PREFIX macros

--- simple_macros.h
+++ simple_macros.h
@@ -2,4 +2,17 @@
 #define SIMPLE_MACROS_HEADER
 
 
+
+
+
+#ifdef  __cplusplus
+        #define  EXTERN_PREFIX extern "C"
+#else
+        #define  EXTERN_PREFIX extern
+#endif
+
+
+
+
+
 #endif //SIMPLE_MACROS_HEADER
