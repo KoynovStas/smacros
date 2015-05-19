62265670dd9f32adf5798ce0b71f78c2e2494587
KoynovStas <skojnov@yandex.ru>
2015-05-19 10:41:48 +0300

    Add template of unit tests for project

---
+++ simple_macros_tests.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#include "unit_tests.h"
+
+
+
+
+int test_1(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    //some code
+
+    return TEST_PASSED;
+}
+
+
+
+
+ptest_func tests[] =
+{
+
+    test_1,
+
+};
+
+
+
+int main(void)
+{
+
+    RUN_TESTS(tests);
+
+    return 0;
+}
+
