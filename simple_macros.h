53f0473a4457fa117e2e2e568465eb797454dbee
KoynovStas <skojnov@yandex.ru>
2016-08-17 15:07:12 +0300

    Add macros container_of

--- simple_macros.h
+++ simple_macros.h
@@ -3,6 +3,8 @@
 
 
 #include <stdint.h>
+#include <stddef.h>  //for offsetof
+
 
 
 
@@ -117,6 +119,17 @@
         #define  FDEBUG_MSG(fp, ...)  ({})
 #endif
 
+
+
+
+
+// container_of - cast a member of a structure out to the containing structure
+// ptr:    the pointer to the member.
+// type:   the type of the container struct this is embedded in.
+// member: the name of the member within the struct.
+//
+#define container_of( ptr, type, member ) \
+    ({  (type *)( (const char *)ptr - offsetof(type, member) );  })
 
 
 
